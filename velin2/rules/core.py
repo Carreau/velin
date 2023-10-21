498d4b2d864bf272c2735ee3d2a53a4e4d45c88f
Justus Magin <keewis@posteo.de>
2023-10-21 23:49:56 +0200

    extend the rule class and add a high-level docstring checker

--- velin2/rules/core.py
+++ velin2/rules/core.py
@@ -1,21 +1,62 @@
-from dataclasses import dataclass
+import attrs
+
+from velin2.context import Context
+from velin2.docstring import cleandoc
+from velin2.parser import get_language, get_parser
+from velin2.rules.violations import Violation
+
+lang_rst = get_language("rst")
+parser_rst = get_parser("rst")
+
 
 rules = {}
 
 
-@dataclass
+@attrs.define
 class Rule:
-    checker: callable
-    description: str
+    checker = attrs.field()
+
+    error_code = attrs.field()
+    description = attrs.field()
+
+    def check(self, tree, context):
+        offending_nodes = self.checker(tree, context)
+        violations = [
+            Violation(
+                path=context.path,
+                docstring_node=context.docstring_node,
+                column_offsets=context.column_offsets,
+                offending_node=node,
+                error_code=self.error_code,
+                description=self.description,
+                suggestions=suggestions,
+            )
+            for node, suggestions in offending_nodes
+        ]
+
+        return violations
 
 
 def register_rule(code, description):
+    """register a validation rule"""
+
     def outer(f):
         if code in rules:
             raise ValueError(f"code {code} exists already. Refusing to overwrite")
 
-        rules[code] = Rule(checker=f, description=description)
+        rules[code] = Rule(checker=f, error_code=code, description=description)
 
         return f
 
     return outer
+
+
+def check_docstring(path, node):
+    cleaned_docstring, column_offsets = cleandoc(node.text.decode())
+    context = Context(path, node, column_offsets)
+
+    tree = parser_rst.parse(cleaned_docstring.encode())
+
+    violations = {code: rule.check(tree, context) for code, rule in rules.items()}
+
+    return violations
