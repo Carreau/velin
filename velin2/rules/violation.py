90e80692b1d3053b3ff86611d471d6bffa2b746e
Justus Magin <keewis@posteo.de>
2023-10-22 15:54:56 +0200

    add a property to calculate the absolute error location

--- velin2/rules/violation.py
+++ velin2/rules/violation.py
@@ -1,7 +1,9 @@
+from functools import cached_property
+
 import attrs
 
 
-@attrs.define
+@attrs.define(slots=False)
 class Violation:
     path = attrs.field()
 
@@ -13,3 +15,26 @@ class Violation:
     description = attrs.field()
 
     suggestions = attrs.field()
+
+    @cached_property
+    def point(self):
+        local_line_index = self.offending_node.start_point[0]
+
+        line_index = self.docstring_node.start_point[0] + local_line_index
+        line_number = line_index + 1
+
+        column_number = (
+            self.offending_node.start_point[1] + self.column_offsets[local_line_index]
+        )
+
+        return line_number, column_number
+
+    def __str__(self):
+        line, column = self.point
+
+        return "\n".join(
+            [
+                f"{self.path}:{line}:{column}: {self.error_code}: {self.description}",
+                self.offending_node.text.decode(),
+            ]
+        )
