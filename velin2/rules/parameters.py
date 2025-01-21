fd7b7c8466116cea3f891c4d317c1679e8a2e02f
Justus Magin <keewis@posteo.de>
2025-01-21 22:31:32 +0100

    check that all listed terms are valid identifiers

--- velin2/rules/parameters.py
+++ velin2/rules/parameters.py
@@ -1,20 +1,59 @@
 from velin2.rules.core import lang_rst, register_rule
 
+query_parameters = """
+(
+  section (title) @title
+  (#any-of? @title "Parameters" "Other Parameters")
+)
+"""
+
+
+def format_type(type_):
+    return type_ if type_ != "capture" else "capture name"
+
+
+def validate_parameter_types(types_, expected_types, predicate):
+    if types_ == expected_types:
+        return
+
+    if len(expected_types) == 1:
+        type_names = map(format_type, types_)
+        expected_type_name = format_type(expected_types[0])
+
+        msg = (
+            f"predicate {predicate} requires exactly one {expected_type_name}."
+            f" Got [{', '.join(type_names)}] instead."
+        )
+    else:
+        type_names = map(format_type, types_)
+        expected_type_names = map(format_type, expected_types)
+        msg = (
+            "Unexpected argument types."
+            f" Expected [{', '.join(expected_type_names)}],"
+            f" but got [{', '.join(type_names)}]."
+        )
+
+    raise TypeError(msg)
+
 
 def custom_predicates(predicate, args, pattern_index, captures):
+    types_ = [type_ for _, type_ in args]
+    values = [value for value, _ in args]
     match predicate:
         case "not-definition-list?":
-            if len(args) != 1 or (type_ := args[0][1]) != "capture":
-                type_name = type_ if type_ != "capture" else "capture name"
-                raise TypeError(
-                    f"predicate {predicate} requires exactly one capture name."
-                    f" Got {args[0][0]} (a {type_name}) instead."
-                )
+            validate_parameter_types(types_, ["capture"], predicate)
 
             # query guarantees that the capture name exists
-            nodes = captures[args[0][0]]
+            nodes = captures[values[0]]
 
-            return all(node.type != "definition_list" for node in nodes)
+            return any(node.type != "definition_list" for node in nodes)
+
+        case "python-identifier?":
+            validate_parameter_types(types_, ["capture"], predicate)
+
+            nodes = captures[values[0]]
+
+            return any(not node.text.decode().isidentifier() for node in nodes)
 
     raise TypeError(f"unknown predicate: {predicate}")
 
@@ -24,14 +63,10 @@ def custom_predicates(predicate, args, pattern_index, captures):
     "Parameters must be formatted as a definition list",
 )
 def check_format_as_definition_list(tree, context):
-    # '((section (title) @title (#eq? @title "Parameters")) (_)+ @content)'
     query = lang_rst.query(
-        """
+        f"""
         (
-          (
-            section (title) @title
-            (#any-of? @title "Parameters" "Other Parameters")
-          )
+          {query_parameters}
           .
           ((_) @node (#not-definition-list? @node)) @content
         )
@@ -42,4 +77,30 @@ def check_format_as_definition_list(tree, context):
 
     violations = [node for node in result.get("content", [])]
     suggestions = [_ for _ in violations]
+    return zip(violations, suggestions)
+
+
+@register_rule(
+    "V110",
+    "Listed parameters must be valid python identifiers.",
+)
+def check_term_is_a_python_identifier(tree, context):
+    query = lang_rst.query(
+        f"""
+        (
+          {query_parameters}
+          .
+          (
+            definition_list
+            (list_item (term) @term (#python-identifier? @term))
+          )
+        )
+        """
+    )
+
+    result = query.captures(tree.root_node, predicate=custom_predicates)
+
+    violations = [node for node in result.get("term", [])]
+    suggestions = [_ for _ in violations]
+
     return zip(violations, suggestions)
--- velin2/tests/test_parameter_rules.py
+++ velin2/tests/test_parameter_rules.py
@@ -86,3 +86,63 @@ def test_format_as_definition_list(docstring, n_violations, rules):
     violations = _check_docstring(tree, context)
 
     assert len(violations) == n_violations, format_violations(violations)
+
+
+@pytest.mark.parametrize(
+    ["docstring", "n_violations"],
+    (
+        pytest.param(
+            """
+            Parameters
+            ----------
+            a : int
+                description
+            """,
+            0,
+            id="parameters-passing",
+        ),
+        pytest.param(
+            """
+            Parameters
+            ----------
+            1a : int
+                description
+            """,
+            1,
+            id="parameters-failing",
+        ),
+        pytest.param(
+            """
+            Other Parameters
+            ----------------
+            a : int
+                description
+            """,
+            0,
+            id="other parameters-passing",
+        ),
+        pytest.param(
+            """
+            Other Parameters
+            ----------------
+            1a : int
+                description
+            """,
+            1,
+            id="other parameters-failing",
+        ),
+    ),
+)
+def test_term_is_a_python_identifier(docstring, n_violations, rules):
+    rules.isolate("V110")
+
+    dummy_node = DummyNode()
+
+    cleaned_docstring, column_offsets = cleandoc(docstring)
+
+    tree = parser_rst.parse(cleaned_docstring.encode())
+    context = Context("<test example>", dummy_node, column_offsets)
+
+    violations = _check_docstring(tree, context)
+
+    assert len(violations) == n_violations, format_violations(violations)
